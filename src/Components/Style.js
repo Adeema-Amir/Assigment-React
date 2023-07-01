dc4236a44614d2ae022671710a0315e063daa9b0
Adeema Amir <adeemaamir1722@gmail.com>
2023-07-01 10:22:41 +0500

    Assigment-React
    Assigment-React

---
+++ src/Components/Style.js
@@ -0,0 +1,26 @@
+const person = {
+    name: 'Gregorio Y. Zara',
+    src: 'https://i.imgur.com/7vQD0fPs.jpg',
+    theme: {
+      backgroundColor: 'black',
+      color: 'yellow ',
+    }
+};
+  
+export default function Style() {
+    return (
+      <div style={person.theme}>
+        <h1>{person.name}s Todos</h1>
+        <img
+          className="avatar"
+          src={person.src}
+          alt="Gregorio Y. Zara"
+        />
+        <ul>
+          <li>Lorem ipsum dolor.</li>
+          <li>Sit amet consectetur.</li>
+          <li>Elit neque quas dolorum.</li>
+        </ul>
+      </div>
+    );
+}
